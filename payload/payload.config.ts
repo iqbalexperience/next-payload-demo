f98b0fd22de30547ef856774e23a85a809de7099
Iqbal <iqbalnawaz072@gmail.com>
2024-03-09 02:21:35 +0500

    initial

--- payload/payload.config.ts
+++ payload/payload.config.ts
@@ -1,30 +1,30 @@
-import { buildConfig } from 'payload/config';
-import path from 'path';
-import { Users } from './collections/Users';
-import { Pages } from './collections/Pages';
-import { MainMenu } from './globals/MainMenu';
-import { cloudStorage } from '@payloadcms/plugin-cloud-storage';
-import { s3Adapter } from '@payloadcms/plugin-cloud-storage/s3';
-import { Media } from './collections/Media';
-import seo from '@payloadcms/plugin-seo';
-import { webpackBundler } from '@payloadcms/bundler-webpack';
+import { buildConfig } from "payload/config";
+import path from "path";
+import { Users } from "./collections/Users";
+import { Pages } from "./collections/Pages";
+import { MainMenu } from "./globals/MainMenu";
+import { cloudStorage } from "@payloadcms/plugin-cloud-storage";
+import { s3Adapter } from "@payloadcms/plugin-cloud-storage/s3";
+import { Media } from "./collections/Media";
+import seo from "@payloadcms/plugin-seo";
+import { webpackBundler } from "@payloadcms/bundler-webpack";
 // import { postgresAdapter } from '@payloadcms/db-postgres';
-import { mongooseAdapter } from '@payloadcms/db-mongodb';
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
 // import { slateEditor } from '@payloadcms/richtext-slate';
-import type { RichTextAdapter } from 'payload/types';
+import type { RichTextAdapter } from "payload/types";
 
 const adapter = s3Adapter({
   config: {
-    endpoint: process.env.NEXT_PUBLIC_S3_ENDPOINT,
+    // endpoint: process.env.NEXT_PUBLIC_S3_ENDPOINT,
     region: process.env.S3_REGION,
     forcePathStyle: true,
     credentials: {
       accessKeyId: process.env.S3_ACCESS_KEY_ID as string,
       secretAccessKey: process.env.S3_SECRET_ACCESS_KEY as string,
-    }
+    },
   },
   bucket: process.env.NEXT_PUBLIC_S3_BUCKET as string,
-})
+});
 
 export function emptyEditor(): RichTextAdapter<any, {}, {}> {
   return {
@@ -33,16 +33,16 @@ export function emptyEditor(): RichTextAdapter<any, {}, {}> {
     outputSchema: () => {
       return {
         items: {
-          type: 'object',
+          type: "object",
         },
-        type: ['array'],
-      }
+        type: ["array"],
+      };
     },
     populationPromise() {
-      return null
+      return null;
     },
     validate: () => true,
-  }
+  };
 }
 
 export default buildConfig({
@@ -52,45 +52,43 @@ export default buildConfig({
   //   }
   // }),
   db: mongooseAdapter({
-    url: process.env.MONGODB_URI as string
+    url: process.env.MONGODB_URI as string,
   }),
   editor: emptyEditor(),
   admin: {
-    bundler: webpackBundler()
+    bundler: webpackBundler(),
   },
   collections: [
     Pages,
     Users,
     Media,
     {
-      slug: 'examples',
+      slug: "examples",
       fields: [
         {
-          name: 'title',
-          type: 'text',
-        }
-      ]
-    }
+          name: "title",
+          type: "text",
+        },
+      ],
+    },
   ],
-  globals: [
-    MainMenu,
-  ],
+  globals: [MainMenu],
   typescript: {
-    outputFile: path.resolve(__dirname, '../payload-types.ts'),
+    outputFile: path.resolve(__dirname, "../payload-types.ts"),
   },
   graphQL: {
-    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+    schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
   },
   plugins: [
     seo({
-      collections: ['pages']
+      collections: ["pages"],
     }),
     cloudStorage({
       collections: {
-        'media': {
+        media: {
           adapter,
           disablePayloadAccessControl: true,
-        }
+        },
       },
     }),
   ],
